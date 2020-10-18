d5ccd5cfccf16924840b73f69c34c2f73555b4c7
Mario Ortiz Manero <marioortizmanero@gmail.com>
2020-10-18 20:39:54 +0200

    update

--- compare_repo.sh
+++ compare_repo.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 # Benchmarks from the maintainer's perspective.
+#
+# 0.10 release - 33d8f770b3d0bcaaccd0c83c0ab0481edb7e5eaa
+# 0.11 release - master
 
 set -e
 
 if [ $# -ne 2 ]; then
-    echo "Usage: $0 OLD_BRANCH NEW_BRANCH"
+    echo "Usage: $0 [BRANCHES..]"
     exit 1
 fi
 
-# Like '33d8f770b3d0bcaaccd0c83c0ab0481edb7e5eaa'
-branch_old=$1
-# Like 'master'
-branch_new=$2
+repo_dir=$(mktemp -d)
+git clone "https://github.com/ramsayleung/rspotify" "$repo_dir"
+rm -rf "$repo_dir"
 
-tmp_dir=$(mktemp -d)
-git clone "https://github.com/ramsayleung/rspotify" "$tmp_dir"
-rm -rf "$tmp_dir"
+for branch in "$@"; do
+    echo ">> BENCHMARKING BRANCH $branch"
+    echo ">> Line count:"
+    tokei "$repo_dir"
+done
--- compare_user.sh
+++ compare_user.sh
@@ -8,11 +8,14 @@ BENCHMARKS="./bench_new_async ./bench_new_blocking ./bench_old_async ./bench_old
 unset CARGO_TARGET_DIR
 
 for bench in $BENCHMARKS; do
+    echo ">> BENCHMARKING $branch"
     cd "$bench"
     echo "$bench"
 
     cargo clean
-    cargo build --release >/dev/null 2>&1
-    ./target/release/$bench
+    echo ">> Compilation benchmark:"
+    cargo build --release
+    echo ">> Runtime benchmark:"
+    time ./target/release/$bench
     cd ..
 done
