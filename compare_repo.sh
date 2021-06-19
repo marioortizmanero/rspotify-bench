4a3aa00dbeaa0b10c074eeeaaa41f820a0815372
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-06-19 15:24:40 +0200

    compare_repo done

--- compare_repo.sh
+++ compare_repo.sh
@@ -12,11 +12,20 @@ if [ $# -ne 2 ]; then
 fi
 
 repo_dir=$(mktemp -d)
-git clone "https://github.com/ramsayleung/rspotify" "$repo_dir"
-rm -rf "$repo_dir"
+git clone "https://github.com/ramsayleung/rspotify" "$repo_dir" >/dev/null 2>&1
+cd "$repo_dir"
 
 for branch in "$@"; do
     echo ">> BENCHMARKING BRANCH $branch"
+    git checkout "$branch" >/dev/null 2>&1
+
     echo ">> Line count:"
     tokei "$repo_dir"
+
+    deps=$(cargo tree -q --edges normal --charset ascii | sed 's/[-|`]//g' | awk '{print $1}' | sort | uniq | wc -l)
+    deps_all=$(cargo tree -q --all-features --edges normal --charset ascii | sed 's/[-|`]//g' | awk '{print $1}' | sort | uniq | wc -l)
+    echo ">> Dependencies in tree: $deps"
+    echo ">> Dependencies in tree with all features: $deps_all"
 done
+
+rm -rf "$repo_dir"
