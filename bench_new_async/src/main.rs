1f30dcfdb6f933637bb72baf012971271dd20546
Mario Ortiz Manero <marioortizmanero@gmail.com>
2021-10-12 21:26:19 +0200

    Update to last version

--- bench_new_async/src/main.rs
+++ bench_new_async/src/main.rs
@@ -1,11 +1,11 @@
-use rspotify::{model::Id, prelude::*, ClientCredsSpotify, Credentials};
+use rspotify::{model::AlbumId, prelude::*, ClientCredsSpotify, Credentials};
 
 #[tokio::main]
 async fn main() {
     let creds = Credentials::from_env().unwrap();
     let mut spotify = ClientCredsSpotify::new(creds);
     spotify.request_token().await.unwrap();
-    let birdy_uri = Id::from_uri("spotify:album:0sNOF9WDwhWunNAHPD3Baj").unwrap();
-    let albums = spotify.album(birdy_uri).await;
+    let birdy_uri = AlbumId::from_uri("spotify:album:0sNOF9WDwhWunNAHPD3Baj").unwrap();
+    let albums = spotify.album(&birdy_uri).await;
     println!("Response: {:?}", albums);
 }
--- bench_new_blocking/src/main.rs
+++ bench_new_blocking/src/main.rs
@@ -1,10 +1,10 @@
-use rspotify::{model::Id, prelude::*, ClientCredsSpotify, Credentials};
+use rspotify::{model::AlbumId, prelude::*, ClientCredsSpotify, Credentials};
 
 fn main() {
     let creds = Credentials::from_env().unwrap();
     let mut spotify = ClientCredsSpotify::new(creds);
     spotify.request_token().unwrap();
-    let birdy_uri = Id::from_uri("spotify:album:0sNOF9WDwhWunNAHPD3Baj").unwrap();
-    let albums = spotify.album(birdy_uri);
+    let birdy_uri = AlbumId::from_uri("spotify:album:0sNOF9WDwhWunNAHPD3Baj").unwrap();
+    let albums = spotify.album(&birdy_uri);
     println!("Response: {:?}", albums);
 }
